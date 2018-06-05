ded62377b3d78a3e3e59f98bdab4c4a5aaca1c5a
Simkhotech <simkhotech@gmail.com>
2018-06-05 15:48:05 +0300

    Добвлены Формы логина и регистрации и страница входа

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,28 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+} from 'react-router-dom';
 
-import WelcomePage from './ChatHeader';
-import Chat from './Chat';
+import WelcomePage from '../containers/WelcomePage';
+import ChatPage from '../containers/ChatPage';
+import configureStore from '../store';
+
+const store = configureStore();
 
 const App = () => (
-  <Router>
-    <Switch>
-      <Route exact path="/" component={WelcomePage} />
-      <Route path="/chat" component={Chat} />
-      <Redirect to="/" />
-    </Switch>
-  </Router>
+  <Provider store={store}>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={WelcomePage} />
+        <Route path="/chat" component={ChatPage} />
+        <Redirect to="/" />
+      </Switch>
+    </Router>
+  </Provider>
 );
 
 export default App;
